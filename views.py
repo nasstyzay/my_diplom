d1d4d860dd49e34451d29c7f2821aec3fe3795aa
nasstyzay <150017948+nasstyzay@users.noreply.github.com>
2025-02-04 11:39:46 +0300

    Update views.py

--- views.py
+++ views.py
@@ -1,39 +1,73 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
 from .models import Shop, Category, Product, ProductInfo, Order, OrderItem, Contact, Parameter, ProductParameter
-from .serializers import ShopSerializer, CategorySerializer, ProductSerializer, ProductInfoSerializer, OrderSerializer, OrderItemSerializer, ContactSerializer, ParameterSerializer, ProductParameterSerializer
+from .serializers import ShopSerializer, CategorySerializer, ProductSerializer, ProductInfoSerializer, OrderSerializer, \
+    OrderItemSerializer, ContactSerializer, ParameterSerializer, ProductParameterSerializer
+from django.http import HttpResponse
+
 
 class ShopViewSet(viewsets.ModelViewSet):
     queryset = Shop.objects.all()
     serializer_class = ShopSerializer
 
+
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
+    @action(detail=False, methods=['get'])
+    def search(self, request):
+        query = request.query_params.get('query', '')
+        queryset = self.queryset.filter(name__icontains=query)
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+
 class ProductInfoViewSet(viewsets.ModelViewSet):
     queryset = ProductInfo.objects.all()
     serializer_class = ProductInfoSerializer
 
+
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
+
+    @action(detail=True, methods=['post'])
+    def confirm_order(self, request, pk=None):
+        order = get_object_or_404(self.queryset, pk=pk)
+        order.status = 'confirmed'
+        order.save()
+        return Response({'status': 'order confirmed'}, status=status.HTTP_200_OK)
+
 
 class OrderItemViewSet(viewsets.ModelViewSet):
     queryset = OrderItem.objects.all()
     serializer_class = OrderItemSerializer
 
+
 class ContactViewSet(viewsets.ModelViewSet):
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
+
 
 class ParameterViewSet(viewsets.ModelViewSet):
     queryset = Parameter.objects.all()
     serializer_class = ParameterSerializer
 
+
 class ProductParameterViewSet(viewsets.ModelViewSet):
     queryset = ProductParameter.objects.all()
     serializer_class = ProductParameterSerializer
+
+def home(request):
+    return HttpResponse("Hello, this is the home page!")
+
